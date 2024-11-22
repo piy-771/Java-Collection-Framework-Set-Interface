cf814d263890e7ab4e353a75e1385bf04aa09fd2
piy-771 <piyushsahu2103@gmail.com>
2024-11-22 21:23:34 +0530

    Initial commit

---
+++ src/TreeSetDemo.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+class TreeSet1{
+    Set set = new TreeSet();
+    public void addRecord(Object object){
+        set.add(object);
+        //System.out.println(set);
+    }
+
+    public  void viewRecord(){
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + "\t");
+        }
+        System.out.println();
+    }
+
+    public void remove(int data){
+        set.remove(data);
+    }
+
+
+    public void searchRecord(int data){
+        if(set.contains(data)){
+            System.out.println(data + " found");
+        }else{
+            System.out.println(data + " not found");
+        }
+    }
+}
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet1 obj = new TreeSet1();
+        obj.addRecord(50);
+        obj.addRecord(40);
+        obj.addRecord(700);
+        obj.addRecord(250);
+        obj.addRecord(700);
+        obj.viewRecord();
+        obj.searchRecord(250);
+        obj.remove(250);
+        obj.searchRecord(250);
+
+    }
+}
